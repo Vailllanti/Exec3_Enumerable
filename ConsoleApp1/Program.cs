b46bfab192a13c15dbd81373a0ff2d86a4702e26
建均 陳 <protosscarrier@gmail.com>
2022-11-07 19:53:29 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var items = Enumerable.Range(1, 10);
+			
+			foreach (var item in items)
+			{ 
+				Console.WriteLine(item); 
+			}
+
+			Console.WriteLine();
+
+			var item2 =Enumerable.Range(10, 6);
+
+			foreach (var item in item2)
+			{
+				Console.WriteLine(item);
+			}
+
+
+
+		}
+	}
+}
